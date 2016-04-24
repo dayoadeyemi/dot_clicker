12ffe0e268e2127c89c419721e64b0b82d5839b0
dayoadeyemi <thedayoadeyemi@gmail.com>
2016-04-24 17:59:49 +0100

    asdfghj

--- app.ts
+++ app.ts
@@ -1,6 +1,17 @@
-﻿/// <reference path="jquery.d.ts" />
-
+/// <reference path="jquery.d.ts" />
+'use strict';
 const FPS = 30;
+
+function randomInt(min: number, max: number){
+    return Math.random() * (max - min) + min;
+}
+
+function bound(num: number, min: number, max: number){
+    if (num < min) return min;
+    else if (num > max) return max;
+    else return num;
+}
+
 class GameObject {
     stage: Stage;
     constructor(stage: Stage) {
@@ -28,7 +39,11 @@ class Vector {
         this.y = y;
         Object.freeze(this);
     }
+    get normal(): Vector{
+        return this.multiply(1 / this.length);
+    }
     add = (v: Vector) => new Vector(v.x + this.x, v.y + this.y);
+    sub = (v: Vector) => new Vector(-v.x + this.x, -v.y + this.y);
     multiply = (a: number) => new Vector(a * this.x, a * this.y);
     xform = (m: Matrix) => new Vector(m.x1 * this.x + m.y1 * this.y, m.x2 * this.x + m.y2 * this.y);
 }
@@ -59,8 +74,10 @@ class MovableGameObject extends GameObject {
         super.update(dt);
         this.position = this.position.add(this.velocity.multiply(dt));
         this.velocity = this.velocity.add(this.acceleration.multiply(dt));
-        this.position = new Vector((this.position.x + this.stage.width) % this.stage.width,
-            (this.position.y + this.stage.height) % this.stage.height);
+        this.position = new Vector(
+            bound(this.position.x, this.radius, this.stage.width - this.radius),
+            bound(this.position.y, this.radius, this.stage.height - this.radius)
+        );
     }
 }
 
@@ -86,11 +103,14 @@ class Stage {
     children: Array<GameObject> = [];
     clicks: number = 0;
     time: number = 8;
+    mouse: Vector = new Vector(0, 0);
     constructor(el: HTMLElement) {
         this.canvas = document.createElement("canvas");
         this.ctx = this.canvas.getContext("2d");
         this.canvas.width = 512;
         this.canvas.height = 480;
+        this.width = this.ctx.canvas.width = window.innerWidth;
+        this.height = this.ctx.canvas.height = window.innerHeight;
         el.appendChild(this.canvas);
         this.canvas.onmousedown = (ev) => {
             this.clicks++;
@@ -99,6 +119,9 @@ class Stage {
                 if (gameObject.isClicked(ev)) gameObject.click();
             })
         };
+        this.canvas.onmousemove = (ev: MouseEvent) => {
+            this.mouse = new Vector(ev.pageX, ev.pageY);
+        }
         new GameText(this, 10, 50, () =>
             `level: ${this.level.toString()}
              \tclicks: ${this.clicks} \t
@@ -151,28 +174,53 @@ class Stage {
         this.time += 5;
         var n = this.level;
         if (this.level <= 10) {
-            new Dot(this, 500, 500, 70 / n + 10);
-        } else if (this.level <= 15) {
-            n -= 10;
-            new Dot(this, 100, 500, 70 / n + 10);
-            new Dot(this, 300, 500, 40);
-        } else if (this.level <= 20) {
-            n -= 15;
-            new Dot(this, 100, 500, 25).velocity = new Vector(100 + n * 70, 0);
-            new Dot(this, 300, 500, 25).velocity = new Vector(100 + n * 70, 0);
-            new Dot(this, 500, 500, 25).velocity = new Vector(100 + n * 70, 0);
+            //new Dot(this, Green);
+            // new Dot(this, Red);
+            // new Dot(this, Blue);
+            new Dot(this, Black);
         } else {
             this.endGame();
         }
     }
 }
 
+class DotConfig {
+    colour: string;
+    randomDir: number;
+    speed: number = 150;
+    radius: number = 40;
+    runAway: number;
+    hideIn: number;
+    teleportIn: number;
+    constructor(options: Object){
+        for (let option in options) this[option] = options[option];
+    }
+}
+
+const Green = new DotConfig({ colour: 'green', randomDir: 1.4, speed: 250 });
+const Red = new DotConfig({ colour: 'red', speed: 0, runAway: 100 });
+const Blue = new DotConfig({ colour: 'blue', speed: 0, hideIn: 0.8 });
+const Black = new DotConfig({ colour: 'black', speed: 0, teleportIn: 2.2, radius: 30 });
+
+
 class Dot extends MovableGameObject {
-    colour = 'green';
-    constructor(stage: Stage, x: number, y: number, r: number) {
-        super(stage, x, y, r);
+    config: DotConfig;
+    hideIn: number;
+    teleportIn: number;
+    colour;
+    constructor(stage: Stage, config: DotConfig) {
+        super(
+            stage, 
+            randomInt(config.radius, stage.width),
+            randomInt(config.radius, stage.height),
+            config.radius
+        );
+        this.config = config;
+        this.hideIn = config.hideIn;
+        this.teleportIn = config.teleportIn;
+        this.colour = config.colour;
+        this.velocity = new Vector(config.speed, 0);
         stage.dotCount++;
-        this.velocity = new Vector(150, 0);
     }
     isClicked(ev: MouseEvent) {
         return this.position.add(new Vector(-ev.pageX, -ev.pageY)).length < this.radius;
@@ -182,11 +230,45 @@ class Dot extends MovableGameObject {
         this.destroy = true;;
     }
     update(dt) {
-        let rotate = Matrix.rot((Math.random() - 0.5)*0.9);
-        this.velocity = this.velocity.xform(rotate);
+        let config = this.config;
+        if (config.randomDir){
+            this.velocity = this.velocity.xform(Matrix.rot((Math.random() - 0.5) * config.randomDir));
+        }
+        if (config.runAway) {
+            let mouseDiff = this.position.sub(this.stage.mouse);
+            let speed = config.runAway / (this.radius/4 + mouseDiff.length)
+            this.velocity = mouseDiff.normal.multiply(speed * config.runAway);
+        }
+        if (config.hideIn) {
+            if (this.hideIn > 0) {
+                this.hideIn -= dt;
+                if (Math.random() < 0.75) {
+                    this.radius++
+                } else {
+                    this.radius--
+                }
+                this.radius = bound(this.radius, config.radius, 2 * config.radius);
+            }
+            else {
+                this.colour = 'white';
+            }
+        }
+        if (config.teleportIn) {
+            if (this.teleportIn > 0) {
+                this.teleportIn -= dt;
+            }
+            else {
+                this.teleportIn = this.config.teleportIn;
+                this.position = new Vector(
+                    randomInt(this.radius, this.stage.width),
+                    randomInt(this.radius, this.stage.height)
+                );
+            }
+        }
         super.update(dt);
     }
     draw() {
+        let config = this.config;      
         this.stage.ctx.beginPath();
         this.stage.ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI, false);
         this.stage.ctx.fillStyle = this.colour;
